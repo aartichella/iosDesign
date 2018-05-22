c3e69301dc36a6149c37f1a990c8743f0e3f6a23
Aarti Chella <aartichella@gmail.com>
2018-05-22 10:18:37 -0400

    adding stuff

--- Profile/Profile/Controller/ViewController.swift
+++ Profile/Profile/Controller/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //added new stuff
+        //trying out github
+        
     }
 
     override func didReceiveMemoryWarning() {
